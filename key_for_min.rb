0801f19af0637155ee15e4681196fe0b3ed61455
Paige Miles <paige@lucas-miles.com>
2020-09-07 01:28:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 10
-  min_key = blake
-  count = 1
+  min_value = nil
+  min_key = nil
+  count = 0
   name_hash.each do |key, value|
     if count == 0
       min_value = value
