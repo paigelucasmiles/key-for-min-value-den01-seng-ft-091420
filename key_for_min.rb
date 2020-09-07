451e548351de81e2c1f90c0ec965acbfbc24139b
Paige Miles <paige@lucas-miles.com>
2020-09-07 01:29:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,13 @@ def key_for_min_value(name_hash)
   min_key = nil
   count = 0
   name_hash.each do |key, value|
-    if count = 0
+    if count == 0
       min_value = value
       min_key = key
       count += 1
     elsif value < min_value
       min_value = value
       min_key = key
-      count += 1
     end
   end
   p min_key
